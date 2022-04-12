3a07f747b4bc68144eb454e8005bb60a0f8c37e7
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-04-12 09:54:32 +0530

    typo : add subscribers

--- src/pages/public/SingleVideo/components/VideoWrapper/VideoWrapper.jsx
+++ src/pages/public/SingleVideo/components/VideoWrapper/VideoWrapper.jsx
@@ -1,15 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import ReactPlayer from 'react-player';
 import { getIcons } from '../../../../../util';
 import { getStringValue } from './../../../../../util/getStringValue';
 import './VideoWrapper.css';
 function VideoWrapper({ youtubeId, video }) {
-  const someRef = useRef();
   return (
     <div className="video-wrapper">
       <ReactPlayer
         width="100%"
-        ref={someRef}
         controls
         url={`https://www.youtube.com/watch?v=${youtubeId}`}
       />
@@ -34,8 +32,8 @@ function VideoWrapper({ youtubeId, video }) {
         </div>
         <div>
           <h4 className="video-creator">{video.creator}</h4>
-          <h4 className="video-subscribers">
-            {getStringValue(video.subscribers)}
+          <h4 className="video-subscribers text-muted">
+            {getStringValue(video.subscribers)} subscribers
           </h4>
           <p>{video.description}</p>
         </div>
