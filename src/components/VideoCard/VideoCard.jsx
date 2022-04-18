4809e3d90e6586bcedf432a4ac5830ddb33351b5
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-04-18 20:15:24 +0530

    feat : made a menu component

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,11 +1,13 @@
 import propTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
 import { useAuth } from '../../context/auth/authContext';
 import { getIcons, getLastPosted, getStringValue } from '../../util';
 import { useNavigate } from 'react-router-dom';
 import { useHistory } from '../../context/history/historyContext';
 import { addToHistory } from '../../service';
+import { addToWatchLater } from '../../service';
 import './VideoCard.css';
+import { useWatchLater } from '../../context/watchLater/watchLaterContext';
 function VideoCard({
   channelThumbnail,
   creator,
@@ -22,6 +24,21 @@ function VideoCard({
 }) {
   const { authState } = useAuth();
   const { historyDispatch } = useHistory();
+  const { watchLaterDispatch } = useWatchLater();
+  const video = {
+    channelThumbnail,
+    creator,
+    description,
+    imageThumbnail,
+    likes,
+    releaseDate,
+    subscribers,
+    title,
+    views,
+    youtubeId,
+    category,
+    _id,
+  };
   const videoClickHandler = async () => {
     const response = await addToHistory(
       {
@@ -44,10 +61,14 @@ function VideoCard({
   };
 
   const navigate = useNavigate();
+  const [display, setDisplay] = useState(false);
   return (
     <>
       <div
         className="video-card"
+        onMouseLeave={() => {
+          setDisplay(false);
+        }}
         onClick={() => {
           videoClickHandler();
           navigate(`videos/${_id}`);
@@ -77,7 +98,33 @@ function VideoCard({
               <span className="text-muted">{getLastPosted(releaseDate)}</span>
             </div>
           </div>
-          <div className="video-menu">{getIcons('VIDEO_MENU', '20px')}</div>
+          <div className="video-menu">
+            <span
+              onClick={e => {
+                e.stopPropagation();
+                setDisplay(prevState => !prevState);
+              }}
+            >
+              {getIcons('VIDEO_MENU', '20px')}
+            </span>
+            <div
+              className={display ? 'menu-options show-menu' : 'menu-options '}
+            >
+              <span
+                className="menu-options-item"
+                onClick={e => {
+                  e.stopPropagation();
+                  addToWatchLater(video, authState.token, watchLaterDispatch);
+                }}
+              >
+                {getIcons('WATCH_LATER', '20px')}Add to Watch Later
+              </span>
+              <span className="menu-options-item">
+                {getIcons('PLAYLIST_ADD', '20px')}save to playlist
+              </span>
+              <span>share</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
