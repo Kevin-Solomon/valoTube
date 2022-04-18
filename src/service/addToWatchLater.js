ea30cd392bb848e2d489dba777efaa4567759fe6
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-04-18 18:54:35 +0530

    chore : add try catch block to network call

--- src/service/addToWatchLater.js
+++ src/service/addToWatchLater.js
@@ -1,15 +1,19 @@
 import axios from 'axios';
 
 const addToWatchLater = async (video, token, watchLaterDispatch) => {
-  const response = await axios({
-    method: 'POST',
-    url: '/api/user/watchlater',
-    data: { video },
-    headers: { authorization: token },
-  });
-  watchLaterDispatch({
-    type: 'ADD_TO_WATCH_LATER',
-    payload: response.data.watchlater,
-  });
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: '/api/user/watchlater',
+      data: { video },
+      headers: { authorization: token },
+    });
+    watchLaterDispatch({
+      type: 'ADD_TO_WATCH_LATER',
+      payload: response.data.watchlater,
+    });
+  } catch (err) {
+    if ('response' in err) console.error(err);
+  }
 };
 export { addToWatchLater };
