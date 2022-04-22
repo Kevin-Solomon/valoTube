9c928c7d852c4cf1b5b9a7b72c85f9dfaa9b3946
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-04-23 02:34:01 +0530

    feat : toast for delete all history

--- src/service/deleteAllFromHistory.js
+++ src/service/deleteAllFromHistory.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 const deleteAllFromHistory = async (token, historyDispatch) => {
   try {
     const response = await axios({
@@ -7,6 +8,15 @@ const deleteAllFromHistory = async (token, historyDispatch) => {
       headers: { authorization: token },
     });
     if (response.status === 200) {
+      toast.error(`All videos have been deleted from your history`, {
+        position: 'top-right',
+        autoClose: true,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
       historyDispatch({ type: 'DELETE_ALL', payload: response.data.history });
     }
   } catch (err) {
