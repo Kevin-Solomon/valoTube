d73b440d903f29baffc475195a69edc761b0cb81
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-04-24 22:11:49 +0530

    style : removed login button after authenticated

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../context/auth/authContext';
 
 function Navbar() {
+  const { authState } = useAuth();
   return (
     <nav className="navbar">
       <div className="logo">
         <h2>valoTube</h2>
       </div>
       <div className="right-nav">
-        <Link to="/login">
-          <button className="primary-btn btn">Login</button>
-        </Link>
+        {authState.token === null ? (
+          <Link to="/login">
+            <button className="primary-btn btn">Login</button>
+          </Link>
+        ) : null}
       </div>
     </nav>
   );
