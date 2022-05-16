cf62f2d2e1ec125b285bc6bc589d38bbeefee875
Kevin-Solomon <kevinsolomon404@gmail.com>
2022-05-16 11:54:16 +0530

    feat : add loading animation

--- src/components/VideoListing/VideoListing.jsx
+++ src/components/VideoListing/VideoListing.jsx
@@ -2,42 +2,45 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './VideoListing.css';
 import VideoCard from '../VideoCard/VideoCard';
+import Loading from '../Loading/Loading';
 function VideoListing({ videos }) {
   return (
     <div className="video-list">
-      {videos.map(
-        ({
-          channelThumbnail,
-          creator,
-          description,
-          imageThumbnail,
-          likes,
-          releaseDate,
-          subscribers,
-          title,
-          views,
-          youtubeId,
-          category,
-          _id,
-        }) => {
-          return (
-            <VideoCard
-              channelThumbnail={channelThumbnail}
-              creator={creator}
-              imageThumbnail={imageThumbnail}
-              title={title}
-              releaseDate={releaseDate}
-              views={views}
-              _id={_id}
-              description={description}
-              likes={likes}
-              subscribers={subscribers}
-              youtubeId={youtubeId}
-              category={category}
-            />
-          );
-        }
-      )}
+      {videos.length === 0
+        ? [...new Array(15)].map(item => <Loading key={item} />)
+        : videos.map(
+            ({
+              channelThumbnail,
+              creator,
+              description,
+              imageThumbnail,
+              likes,
+              releaseDate,
+              subscribers,
+              title,
+              views,
+              youtubeId,
+              category,
+              _id,
+            }) => {
+              return (
+                <VideoCard
+                  channelThumbnail={channelThumbnail}
+                  creator={creator}
+                  imageThumbnail={imageThumbnail}
+                  title={title}
+                  releaseDate={releaseDate}
+                  views={views}
+                  _id={_id}
+                  description={description}
+                  likes={likes}
+                  subscribers={subscribers}
+                  youtubeId={youtubeId}
+                  category={category}
+                />
+              );
+            }
+          )}
     </div>
   );
 }
